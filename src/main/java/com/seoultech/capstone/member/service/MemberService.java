a381e46308fbfafcd5922ca8ae85e721efe3dbd3
jdyj <jdyj@naver.com>
2022-11-25 20:18:54 +0900

    [Refactor] import문 정리

--- src/main/java/com/seoultech/capstone/member/service/MemberService.java
+++ src/main/java/com/seoultech/capstone/member/service/MemberService.java
@@ -1,28 +1,15 @@
 package com.seoultech.capstone.member.service;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.seoultech.capstone.TokenDto;
 import com.seoultech.capstone.exception.NotExistMemberException;
-import com.seoultech.capstone.friend.Friend;
 import com.seoultech.capstone.friend.service.FriendService;
-import com.seoultech.capstone.jwt.TokenProvider;
 import com.seoultech.capstone.member.Member;
-import com.seoultech.capstone.member.Role;
 import com.seoultech.capstone.member.MemberRepository;
-import io.vavr.control.Either;
-import java.util.List;
-import java.util.Optional;
+
+
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -33,8 +20,6 @@ public class MemberService {
   private final FriendService friendService;
 
   public MyPage findMyPage(String memberId) {
-
-
 
     return null;
   }
