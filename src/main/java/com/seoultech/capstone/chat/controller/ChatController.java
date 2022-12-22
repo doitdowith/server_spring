fff4526735378db8a8335de5c7f7abbd899c79a1
jdyj <jdyj@naver.com>
2022-12-23 01:03:08 +0900

    [FIX] ChatList 오류 수정

--- src/main/java/com/seoultech/capstone/chat/controller/ChatController.java
+++ src/main/java/com/seoultech/capstone/chat/controller/ChatController.java
@@ -1,13 +1,28 @@
 package com.seoultech.capstone.chat.controller;
 
+import static com.seoultech.capstone.config.jwt.JwtFilter.AUTHORIZATION_HEADER;
+import static com.seoultech.capstone.config.jwt.JwtFilter.BEARER_PREFIX;
+
 import com.seoultech.capstone.chat.dto.ChatListAllResponse;
 import com.seoultech.capstone.chat.service.ChatService;
+import com.seoultech.capstone.config.jwt.JwtConfig;
 import com.seoultech.capstone.config.login.Auth;
 import com.seoultech.capstone.member.Member;
 import com.seoultech.capstone.member.service.MemberService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
+import java.security.Key;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +39,38 @@ public class ChatController {
   private final MemberService memberService;
 
   @GetMapping("/{roomId}")
-  public ResponseEntity<ChatListAllResponse> chatListAll(@ApiIgnore @Auth String memberId,
+  public ResponseEntity<ChatListAllResponse> chatListAll(HttpServletRequest servletRequest,
       @PathVariable String roomId) {
+    String memberId = validateToken(resolveToken(servletRequest));
+
     Member member = memberService.findMemberById(memberId);
     return ResponseEntity.ok()
         .body(ChatListAllResponse.from(chatService.listAll(member, roomId)));
+  }
+
+  private String resolveToken(HttpServletRequest request) {
+    String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+      return bearerToken.substring(7);
+    }
+    return null;
+  }
+
+  private String validateToken(String token) {
+
+    byte[] decode = Decoders.BASE64.decode(JwtConfig.JWT_SECRET);
+    Key key = Keys.hmacShaKeyFor(decode);
+
+    try {
+      Claims claims = Jwts.parserBuilder()
+          .setSigningKey(key)
+          .build()
+          .parseClaimsJws(token)
+          .getBody();
+      return claims.get("jti", String.class);
+    } catch (SecurityException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+      throw e;
+    }
   }
 
 
