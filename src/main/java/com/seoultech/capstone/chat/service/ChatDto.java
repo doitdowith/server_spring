26ae51fb4c7b970f4182283f645485daf56e656a
jdyj <jdyj@naver.com>
2022-12-28 17:50:53 +0900

    [FIX] 친구 수 오류 수정

--- src/main/java/com/seoultech/capstone/chat/service/ChatDto.java
+++ src/main/java/com/seoultech/capstone/chat/service/ChatDto.java
@@ -4,6 +4,8 @@ import com.seoultech.capstone.chat.Chat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.format.DateTimeFormatter;
+
 @Getter
 @AllArgsConstructor
 public class ChatDto {
@@ -18,17 +20,19 @@ public class ChatDto {
 
   private String certificationImage;
 
+  private String time;
+
   public static ChatDto from(Chat chat, boolean isMe) {
 
     if (chat.getImage() == null) {
       return new ChatDto(chat.getMember().getProfileImage().getStoreFileName(),
               chat.getMember().getName(), isMe,
-              chat.getContent(), null);
+              chat.getContent(), null, chat.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
     }
 
     return new ChatDto(chat.getMember().getProfileImage().getStoreFileName(),
         chat.getMember().getName(), isMe,
-        chat.getContent(), chat.getImage().getStoreFileName());
+        chat.getContent(), chat.getImage().getStoreFileName(), chat.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
   }
 
 }
