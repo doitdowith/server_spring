a0cf4fb1d61d8c6cacea0471a2a12c954180874d
jdyj <jdyj@naver.com>
2022-12-28 17:00:00 +0900

    [ADD] 인증 이미지 추가

--- src/main/java/com/seoultech/capstone/chat/service/ChatDto.java
+++ src/main/java/com/seoultech/capstone/chat/service/ChatDto.java
@@ -16,10 +16,19 @@ public class ChatDto {
 
   private String contents;
 
+  private String certificationImage;
+
   public static ChatDto from(Chat chat, boolean isMe) {
+
+    if (chat.getImage() == null) {
+      return new ChatDto(chat.getMember().getProfileImage().getStoreFileName(),
+              chat.getMember().getName(), isMe,
+              chat.getContent(), null);
+    }
+
     return new ChatDto(chat.getMember().getProfileImage().getStoreFileName(),
         chat.getMember().getName(), isMe,
-        chat.getContent());
+        chat.getContent(), chat.getImage().getStoreFileName());
   }
 
 }
